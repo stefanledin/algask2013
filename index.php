2607560cc0fe48a74506d14ca2d19d5c5cc0625b
Stefan Ledin <stefan.ledin@gmail.com>
2013-02-05 06:16:25 +0100

    Post meta

--- index.php
+++ index.php
@@ -81,7 +81,12 @@
 								<article id="<?php the_ID();?>" <?php post_class();?>>
 									<?php the_post_thumbnail(); ?>
 									<h2><?php the_title();?></h2>
+                                    <div class="post-meta">
+                                        <time pubdate><?php _e('Publicerat: '); the_time('Y-m-d H:i');?></time>
+                                        <?php the_category('title_li='); ?>
+                                    </div>
 									<?php the_content('Läs mer »');?>
+                                    <span class="byline"><?php _e('Skrivet av: '); the_author();?></span>
 								</article>
 							<?php endwhile; endif;?>
 	        			</div>
