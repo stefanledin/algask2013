524eb641ee6637b4a6453578218ffa281800f4f7
Stefan Ledin <stefan.ledin@gmail.com>
2013-02-07 21:13:20 +0100

    Body classes

--- header.php
+++ header.php
@@ -35,7 +35,7 @@
         <script src="<?php bloginfo('template_directory');?>/js/vendor/modernizr-2.6.2.min.js"></script>
         <?php wp_head(); ?>
     </head>
-    <body>
+    <body <?php body_class()?>>
         <!--[if lt IE 7]>
             <p class="chromeframe">You are using an <strong>outdated</strong> browser. Please <a href="http://browsehappy.com/">upgrade your browser</a> or <a href="http://www.google.com/chromeframe/?redirect=true">activate Google Chrome Frame</a> to improve your experience.</p>
         <![endif]-->
