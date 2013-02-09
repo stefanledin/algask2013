a46048a8eb78c2439f026668c22f929cf4e277a9
Stefan Ledin <stefan.ledin@gmail.com>
2013-02-10 00:52:21 +0100

    Lagt till toggleClass för sökikonen

--- js/main.js
+++ js/main.js
@@ -1,1 +1,30 @@
+var App = {} || App;
+
+App.init = function () {
+	this.events.toggleSearchField();
+};
+
+App.events =  {
+	
+	toggleSearchField: function () {
+		document.querySelector('i.icon-search').addEventListener('click', function () {
+
+			var classes, el;
+			
+			el = this.parentNode;
+			classes = el.className;
+			
+			if (classes.search('closed') !== -1) {
+				classes = classes.replace('closed', 'open');
+			} else {
+				classes = classes.replace('open', 'closed');
+			}
+
+			el.className = classes;
+		});
+	}
+
+}
+
+App.init();
 
