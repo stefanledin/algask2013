690887e07a0fb5977e47d3a49a85f7cae58a0c95
Stefan Ledin <stefan.ledin@gmail.com>
2013-02-10 14:23:30 +0100

    Bytt h2 till h1

--- page.php
+++ page.php
@@ -30,7 +30,7 @@
 				<?php if ( have_posts() ) : while ( have_posts() ) : the_post(); ?>
 					<article id="<?php the_ID();?>" <?php post_class();?>>
 						<?php the_post_thumbnail(); ?>
-						<h2><?php the_title();?></h2>
+						<h1><?php the_title();?></h1>
 						<?php the_content();?>
 					</article>
 				<?php endwhile; endif;?>
