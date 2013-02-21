7e1423da793c85371c0ac88a93ad3ab344a1b494
Stefan Ledin <stefan.ledin@gmail.com>
2013-02-21 20:47:57 +0100

    Fotboll och Herrlaget är aktiva i menyn även när man är inne på single-spelare.php

--- single-spelare.php
+++ single-spelare.php
@@ -4,45 +4,47 @@
 	<div class="col1-border"></div>
 	<div class="col2-border"></div>
 	<div class="topborder"></div>
-	<div class="row clearfix">
+	<div class="row-fluid">
+		<div class="inner">
+			<nav class="span2">
+				<ul class="submenu">
+				<?php
+					wp_list_pages('title_li=&child_of=5')
+				?>
+				</ul>
+			</nav>
 
-		<nav class="col col2 first-col">
-			<ul class="submenu">
-			<?php
-				wp_list_pages('title_li=&child_of=5')
-			?>
-			</ul>
-		</nav>
+			<section class="span6">
+				<div class="inner">
+					<?php if ( have_posts() ) : while ( have_posts() ) : the_post(); ?>
+						<article id="<?php the_ID();?>" <?php post_class();?>>
+							<h1><?php the_title();?></h1>
+							<figure>
+								<?php the_post_thumbnail('thumbnail'); ?>
+							</figure>
+							<?php $playerInfo = get_field('information');?>
+							<?php if ($playerInfo) : ?>
+							<table class="responsive">
+								<tbody>
+									<?php
+										foreach ($playerInfo as $info) {
+											$output .= '<tr><td>'.$info['fraga'].':</td><td>'.$info['svar'].'</td></tr>';
+										}
+										echo $output;
+									?>
+								</tbody>
+							</table>
+							<?php endif; ?>
+						</article>
+					<?php endwhile; endif;?>
+				</div>
+			</section>
 
-		<section class="col col6">
-			<div class="inner">
-				<?php if ( have_posts() ) : while ( have_posts() ) : the_post(); ?>
-					<article id="<?php the_ID();?>" <?php post_class();?>>
-						<h1><?php the_title();?></h1>
-						<figure>
-							<?php the_post_thumbnail('thumbnail'); ?>
-						</figure>
-						<?php $playerInfo = get_field('information');?>
-						<?php if ($playerInfo) : ?>
-						<table class="responsive">
-							<tbody>
-								<?php
-									foreach ($playerInfo as $info) {
-										$output .= '<tr><td>'.$info['fraga'].':</td><td>'.$info['svar'].'</td></tr>';
-									}
-									echo $output;
-								?>
-							</tbody>
-						</table>
-						<?php endif; ?>
-					</article>
-				<?php endwhile; endif;?>
-			</div>
-		</section>
+			<aside class="span4" role="complementary">
+				<?php get_sidebar(); ?>
+			</aside>
 
-		<aside class="col col4 last-col" role="complementary">
-			<?php get_sidebar(); ?>
-		</aside>
+		</div>
 
 	</div><!-- eo row -->
 
