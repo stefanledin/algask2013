2c6c85181c424924a2392f26182a83967884b878
Stefan Ledin <stefan.ledin@gmail.com>
2013-02-21 18:34:28 +0100

    Undermenyer visas nu på rätt sätt i alla lägen

--- page.php
+++ page.php
@@ -9,14 +9,15 @@
 			<nav class="submenu-container span2">
 				<ul class="submenu">
 				<?php
-					if (!empty($post->ancestors[1])) {
+					if (empty($post->ancestors[2]) && isset($post->ancestors[1])) {
 						wp_list_pages('title_li=&child_of='.$post->ancestors[1]);
+					} elseif(!empty($post->ancestors[2])) {
+						wp_list_pages('title_li=&child_of='.$post->ancestors[2]);
 					} elseif($post->post_parent !== $post->ancestors[0]) {
 						wp_list_pages('title_li=&child_of='.$post->ID);
 					} else {
 						wp_list_pages('title_li=&child_of='.$post->post_parent);
 					}
-					echo $subpage;
 
 				?>
 				</ul>
