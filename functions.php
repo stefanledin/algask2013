61806b81f308e62d3a038a457c6f2c9daab4cba5
Stefan Ledin <stefan.ledin@gmail.com>
2013-04-07 13:31:02 +0200

    Ladda in stilmallen och scriptfilen

--- functions.php
+++ functions.php
@@ -5,10 +5,16 @@
 	 */
 	function load_scripts()
 	{
+		/*
+		Ladda stylesheet 
+		 */
+		wp_enqueue_style( 'stylesheet', get_template_directory_uri() . '/css/style-min.css', null, null, null );
+
+		/*
+		Ladda scripts
+		 */
 		wp_enqueue_script('jquery');
-		wp_enqueue_script('plugins', get_template_directory_uri().'/js/plugins.js', null, null, true);
-		wp_enqueue_script('bootstrap', get_template_directory_uri().'/js/bootstrap.min.js', null, null, true);
-		wp_enqueue_script('main', get_template_directory_uri().'/js/main.js', null, null, true);
+		wp_enqueue_script('plugins', get_template_directory_uri().'/js/main-min.js', null, null, true);
 	}
 	add_action('wp_enqueue_scripts', 'load_scripts');
 
