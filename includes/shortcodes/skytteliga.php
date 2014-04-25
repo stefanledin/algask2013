3fe78d7ca4f98d64734c31b78b6179dba1ccaee6
stefanledin <stefanledin@gmail.com>
2014-04-25 08:09:18 +0200

    Ändrade så att man kan få ut den totala skytteligan för hela säsongen t.ex.

--- includes/shortcodes/skytteliga.php
+++ includes/shortcodes/skytteliga.php
@@ -7,38 +7,48 @@ function orderGoals( $a, $b ) {
 }
 function shortcode_skytteliga ($atts) {
 	extract($atts);
-	$games = get_posts( array(
+	$args = array(
 		'post_type' => 'matcher',
-		'tax_query' => array(
-			'relation' => 'AND',
-			array(
-				'field' => 'slug',
-				'taxonomy' => 'serie',
-				'terms' => $serie
-			),
-			array(
-				'field' => 'slug',
-				'taxonomy' => 'sasong',
-				'terms' => $sasong
-			)
-		)
-	) );
+		'posts_per_page' => -1,
+		'tax_query' => array()
+	);
+	if ($serie && $sasong) {
+		$args['tax_query']['relation'] = 'AND';
+	}
+	if ($serie) {
+		$args['tax_query'][] = array(
+			'field' => 'slug',
+			'taxonomy' => 'serie',
+			'terms' => $serie
+		);
+	}
+	if ($sasong) {
+		$args['tax_query'][] = array(
+			'field' => 'slug',
+			'taxonomy' => 'sasong',
+			'terms' => $sasong
+		);
+	}
+	$games = get_posts( $args );
 	$playerData = array();
 	foreach ($games as $game) {
 		$scorers = get_field('goals', $game->ID);
-		foreach ($scorers as $scorer) {
-			if ( $playerData[$scorer['player'][0]] ) {
-				$goals = $playerData[$scorer['player'][0]];
-				$playerData[$scorer['player'][0]] = $goals + 1;
-			} else {
-				$playerData[$scorer['player'][0]] = 1;
-			}			
+		if ($scorers) {
+			foreach ($scorers as $scorer) {
+				if ( $playerData[$scorer['player'][0]] ) {
+					$goals = $playerData[$scorer['player'][0]];
+					$playerData[$scorer['player'][0]] = $goals + 1;
+				} else {
+					$playerData[$scorer['player'][0]] = 1;
+				}			
+			}
 		}
 	}
 	$players = array();
 	foreach ($playerData as $id => $goals) {
 		$player = get_post( $id );
 		array_push($players, array(
+			'id' => $id,
 			'name' => $player->post_title,
 			'goals' => $goals
 		));
@@ -54,7 +64,7 @@ function shortcode_skytteliga ($atts) {
 		$output .= '<tbody>';
 			foreach ( $players as $player) :
 				$output .= '<tr>';
-					$output .= '<td>'.$player['name'].'</td>';
+					$output .= '<td><a href="'.get_permalink($player['id']).'">'.$player['name'].'</a></td>';
 					$output .= '<td>'.$player['goals'].'</td>';
 				$output .= '</tr>';
 			endforeach;
